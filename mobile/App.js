bd239520b396df75b828804fa6875d9df696196f
johnjohn123456 <johnaf123456@gmail.com>
2018-03-26 15:00:07 +0200

    standar front

--- mobile/App.js
+++ mobile/App.js
@@ -1,69 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native';
+import Expo from 'expo';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const GEOLOCATION_OPTIONS = { enableHighAccuracy: true, timeout: 20000, maximum: 1000};
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      location: { coords: {latitude: 0, longitude: 0}},
+    };
+
+    this.locationChanged = this.locationChanged.bind(this);
+
+  }
+
+  componentWillMount() {
+    Location.watchPositionAsync(GEOLOCATION_OPTIONS, this.locationChanged);
+  }
+
+  locationChanged  = (location) => {
+    region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.05
+    },
+    this.setState({location, region})
+  }
+
   render() {
     return (
-       <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <Image
-          style={styles.image}
-          source={require('./logos/sf-v-logo.png')}
-          />
-        </View>  
-        <View style={styles.connected}>
-          <Text>Connected</Text>
-        </View>  
-        <View style={styles.textContainer}>
-          <Text>MAC:FD:4F:5A:54:D8</Text> 
-        </View>
-      </View>
+      <Expo.MapView
+        style={{ flex: 0.5 }}
+        showsUserLocation={true}
+        region={this.state.region}
+      />    
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  connected: {
-    flex: 1,
-    justifyContent: 'space-around'
-  },
-  imageContainer: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  image: {
-    resizeMode: 'contain',
-    width: 200
-  },
-  textContainer: {
-    flex: 2,
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  } 
-});
+Expo.registerRootComponent(App);
 
