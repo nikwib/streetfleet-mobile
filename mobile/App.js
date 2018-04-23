3c210f3b4bbd59b29ac84833c7a9c31417eaad4c
johnjohn123456 <johnaf123456@gmail.com>
2018-04-23 19:12:03 +0300

    front without expo

--- mobile/App.js
+++ mobile/App.js
@@ -12,13 +12,10 @@ import {
   View
 } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
+import BackgroundGeolocation from "react-native-background-geolocation";
+import { PermissionsAndroid } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -26,18 +23,73 @@ export default class App extends Component<Props> {
     super();
     this.state ={
       mac:'',
-      lat:'',
-      long:''
+      location: {coords:{latitude:0, longitude:0}},
     }
-    DeviceInfo.getMACAddress().then(mac => {
-      this.setState({mac:mac});
+  DeviceInfo.getMACAddress().then(mac => {
+    this.setState({mac:mac});
+  })
+  this.watchLocation();
+}
+
+postCoordinates() {
+  fetch('https://streetfleet-mainserver.herokuapp.com/api/v1/vehicle/location',{
+    method:'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "time":Date.now(),
+      "latitude": this.state.location.coords.latitude,
+      "longitude": this.state.location.coords.longitude,
+      "mac_address":this.state.mac
     })
+  })
+  .catch(e => {
+    console.log(e);
+  })
+}
+
+  getLocation = async () => {
+  test++
+  const status = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+  console.log('passed');
+  if (status !== PermissionsAndroid.RESULTS.GRANTED) {
+    this.setState({ errorMessage: 'Permission to access location was denied' });
   }
+
+  const location = await navigator.geolocation.watchPosition((position) => {
+    console.log(position.coords);
+    this.setState({
+      location:position,
+    });
+  }, err => {
+    console.log(err);
+    alert('fetching the position fail');
+  }, {enableHighAccuracy: true, timeout: 20000, maximumAge: 0, distanceFilter: 1});
+};
+
+  watchLocation = async () => {
+    console.log('hey yo');
+    //TODO: use watch coordinates method
+
+    await this.getLocation();
+    this.postCoordinates();
+
+   console.log('this',this.setTimeoutId);
+};
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          {this.state.mac}
+          Mac_Address:{this.state.mac}
+        </Text>
+        <Text>
+          latitude:{this.state.location.coords.latitude}
+        </Text>
+        <Text>
+          longitude:{this.state.location.coords.longitude}
+          test:{this.state.test}
         </Text>
       </View>
     );
@@ -55,10 +107,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
